a44ae806bbe6973ae788bd8f4dce58f5e50bc6b4
Uche Victory <uchevictory4@gmail.com>
2022-06-24 06:33:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const about_me ={
+    name: 'Uche Victory',
+    height: "5.8 inches",
+    country: 'Nigeria'
+}
+// alert('My name is' + about_me.name , 'i am' + about_me.height , 'tall. I am from' + about_me.country.);
+// alert('My name is' ${about_me.name} , 'i am' ${about_me.height} , 'tall. I am from' ${about_me.country}.);
+alert(' My name is ' + about_me.name  + '\n I am ' + about_me.height + ' tall.'  + '\n I am from ' + about_me.country )
